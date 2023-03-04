8a8cb03eb7d10e3132c4bad2f624c2e64785bad5
kyle-zamudio19 <kylezamudio19@gmail.com>
2023-03-04 09:25:49 +0800

    commented out fun isLeapYear in activity 2

--- app/src/main/java/com/example/week2activities/week2/Activity2.kt
+++ app/src/main/java/com/example/week2activities/week2/Activity2.kt
@@ -11,15 +11,15 @@ fun main() {
     isLeapYear(year)
 }
 
-fun isLeapYear(year: Int) {
-    val leapYear = " is a Leap Year"
-    val notALeapYear = " is not a Leap Year"
-
-    if (year % 4 == 0) {
-        println(leapYear)
-    } else if (year % 100 == 0) {
-        println(leapYear)
-    } else if (year % 400 == 0) {
-        println(notALeapYear)
-    } else println(notALeapYear)
-}
+//fun isLeapYear(year: Int) {
+//    val leapYear = " is a Leap Year"
+//    val notALeapYear = " is not a Leap Year"
+//
+//    if (year % 4 == 0) {
+//        println(leapYear)
+//    } else if (year % 100 == 0) {
+//        println(leapYear)
+//    } else if (year % 400 == 0) {
+//        println(notALeapYear)
+//    } else println(notALeapYear)
+//}
